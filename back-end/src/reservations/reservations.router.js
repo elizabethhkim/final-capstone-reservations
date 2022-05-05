b373d8b0f974e09413e34352e344dc7c66b0edba
Elizabeth Kim <ehkim129@gmail.com>
2022-05-05 13:41:01 -0700

    to update reservations router

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,6 +1,22 @@
-const router = require("express").Router();
-const controller = require("./reservations.controller");
+const router = require("express").Router()
+const controller = require("./reservations.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/").get(controller.list);
+router
+    .route("/:reservation_id/status")
+    .put(controller.updateStatus)
+    .all(methodNotAllowed)
 
-module.exports = router;
+router
+    .route("/:reservation_id")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed)
+
+router
+    .route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed)
+
+module.exports = router
