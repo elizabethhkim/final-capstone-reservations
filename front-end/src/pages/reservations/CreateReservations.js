99d185e8cbf318728e1760743e305f52f501d8e9
Elizabeth Kim <ehkim129@gmail.com>
2022-05-06 00:14:52 -0700

    update create/edit reservation forms

---
+++ front-end/src/pages/reservations/CreateReservations.js
@@ -0,0 +1,10 @@
+import ReservationForm from "../../Components/ReservationForm";
+
+export default function CreateReservation() {
+  return (
+    <>
+      <h1 className="text-center">Create Reservation</h1>
+      <ReservationForm />
+    </>
+  );
+}
---
+++ front-end/src/pages/reservations/EditReservation.js
@@ -0,0 +1,10 @@
+import ReservationForm from "../../Components/ReservationForm";
+
+export default function EditReservation() {
+  return (
+    <>
+      <h1 className="text-center">Edit Reservation</h1>
+      <ReservationForm />
+    </>
+  );
+}
