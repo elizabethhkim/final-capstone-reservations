4d7bafd20ec9d80f3f68813a04331a9fa4b33a1f
Elizabeth Kim <ehkim129@gmail.com>
2022-05-06 03:26:23 -0700

    update formatting

--- front-end/src/layout/DateNav.js
+++ front-end/src/layout/DateNav.js
@@ -11,9 +11,9 @@ export default function DateNav({ date, next, today, prev, setDate }) {
 
   return (
     <div className="date-nav">
-      <button onClick={() => handleDateNav(prev(date))} type="button" className="btn med-neutral-bg light light-hover"><i className="fas fa-backward"></i> Previous</button>
+      <button onClick={() => handleDateNav(prev(date))} type="button" className="btn med-neutral-bg light"><i className="fas fa-backward"></i> Previous</button>
       <button onClick={() => handleDateNav(today())} type="button" className="btn dark med-green-bg">Today</button>
-      <button onClick={() => handleDateNav(next(date))} type="button" className="btn med-neutral-bg light light-hover">Next <i className="fas fa-forward"></i></button>
+      <button onClick={() => handleDateNav(next(date))} type="button" className="btn med-neutral-bg light">Next <i className="fas fa-forward"></i></button>
     </div>
   )
 }
