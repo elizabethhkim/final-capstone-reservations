6045ba9591d07828b6cd7902532e862875faa7d6
Elizabeth Kim <ehkim129@gmail.com>
2022-05-06 00:14:18 -0700

    update dashboard

--- front-end/src/pages/dashboard/Dashboard.js
+++ front-end/src/pages/dashboard/Dashboard.js
@@ -1,38 +1,93 @@
 import React, { useEffect, useState } from "react";
 import { listReservations, listTables } from "../../utils/api";
-import {today, next, previous} from "../../utils/date-time"
+import { today, next, previous } from "../../utils/date-time";
+import useQuery from "../../utils/useQuery";
 import ErrorAlert from "../../layout/ErrorAlert";
+import ReservationsList from "../Reservations/ReservationsList";
+import DateNav from "../../layout/DateNav";
+import dateFormat from "../../utils/dateFormat";
+import TablesList from "../Tables/TablesList";
+import "./dashboard.css";
 
-/**
- * Defines the dashboard page.
- * @param date
- *  the date for which the user wants to view reservations.
- * @returns {JSX.Element}
- */
-function Dashboard({ date }) {
+function Dashboard() {
+  const query = useQuery();
+  const [date, setDate] = useState(query.get("date") || today());
+  const [error, setError] = useState(null);
   const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
+  const [tables, setTables] = useState([]);
 
   useEffect(loadDashboard, [date]);
 
   function loadDashboard() {
-    const abortController = new AbortController();
-    setReservationsError(null);
-    listReservations({ date }, abortController.signal)
-      .then(setReservations)
-      .catch(setReservationsError);
-    return () => abortController.abort();
+    const ac = new AbortController();
+    setError(null);
+    async function loadReservations() {
+      try {
+        const loadedReservations = await listReservations({ date }, ac.signal);
+        setReservations([...loadedReservations]);
+        setError(null);
+      } catch (err) {
+        setError(err);
+      }
+    }
+    async function loadTables() {
+      try {
+        setError(null);
+        const loadedTables = await listTables(ac.signal);
+        setTables([...loadedTables]);
+      } catch (err) {
+        setError(err);
+      }
+    }
+
+    loadReservations();
+    loadTables();
+    return () => ac.abort();
   }
 
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+    <>
+      <h1 className="title">Dashboard</h1>
+      <ErrorAlert error={error} />
+      <div className="dashboard-content">
+        <section id="reservations">
+          <div className="section-heading">
+            <h2 className="sub-title">
+              <i className="fas fa-user-friends accent1"></i> Reservations
+            </h2>
+            <p className="reservations-date">for {dateFormat(date)}</p>
+          </div>
+
+          <DateNav
+            date={date}
+            next={next}
+            today={today}
+            prev={previous}
+            setDate={setDate}
+          />
+
+          {reservations.length < 1 && <h3>No reservations for this date</h3>}
+
+          <ReservationsList
+            reservations={reservations}
+            setError={setError}
+            date={date}
+            next={next}
+            today={today}
+            prev={previous}
+            setDate={setDate}
+          />
+        </section>
+        <section id="tables">
+          <div className="section-heading">
+            <h2 className="sub-title">
+              <i className="fas fa-utensils accent2"></i> {""} Tables
+            </h2>
+          </div>
+          <TablesList tables={tables} setError={setError} />
+        </section>
       </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
-    </main>
+    </>
   );
 }
 
